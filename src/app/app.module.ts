f5ddb69d8f718f6fcab1d6efbe755561ca5dd2c0
josearangos <jose.arangos@udea.edu.co>
2018-12-13 12:12:11 -0500

    add pago

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,33 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { TarjetaProductoInicialComponent } from './componentes/tarjeta-producto-inicial/tarjeta-producto-inicial.component';
-import { TarjetaProductoParticipandoComponent } from './componentes/tarjeta-producto-participando/tarjeta-producto-participando.component';
-import { TarjetaProductoMetaCumplidaComponent } from './componentes/tarjeta-producto-meta-cumplida/tarjeta-producto-meta-cumplida.component';
-import { TarjetaProductoCompradoComponent } from './componentes/tarjeta-producto-comprado/tarjeta-producto-comprado.component';
-import { DetalleProductoComponent } from './componentes/detalle-producto/detalle-producto.component';
-import { CarritoCompraComponent } from './paginas/carrito-compra/carrito-compra.component';
-import { PagoComponent } from './paginas/pago/pago.component';
-import { HeaderComponent } from './componentes/header/header.component';
-import { FooterComponent } from './componentes/footer/footer.component';
-import { PincipalComponent } from './paginas/pincipal/pincipal.component';
+import { TarjetaProductoInicialComponent } from "./componentes/tarjeta-producto-inicial/tarjeta-producto-inicial.component";
+import { TarjetaProductoParticipandoComponent } from "./componentes/tarjeta-producto-participando/tarjeta-producto-participando.component";
+import { TarjetaProductoMetaCumplidaComponent } from "./componentes/tarjeta-producto-meta-cumplida/tarjeta-producto-meta-cumplida.component";
+import { TarjetaProductoCompradoComponent } from "./componentes/tarjeta-producto-comprado/tarjeta-producto-comprado.component";
+import { DetalleProductoComponent } from "./componentes/detalle-producto/detalle-producto.component";
+import { CarritoCompraComponent } from "./paginas/carrito-compra/carrito-compra.component";
+import { PagoComponent } from "./paginas/pago/pago.component";
+import { HeaderComponent } from "./componentes/header/header.component";
+import { FooterComponent } from "./componentes/footer/footer.component";
+import { PincipalComponent } from "./paginas/pincipal/pincipal.component";
+import { NgSimpleAutocompleteModule } from "ng-simple-autocomplete2";
 
 @NgModule({
-  declarations: [AppComponent, TarjetaProductoInicialComponent, TarjetaProductoParticipandoComponent, TarjetaProductoMetaCumplidaComponent, TarjetaProductoCompradoComponent, DetalleProductoComponent, CarritoCompraComponent, PagoComponent, HeaderComponent, FooterComponent, PincipalComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    TarjetaProductoInicialComponent,
+    TarjetaProductoParticipandoComponent,
+    TarjetaProductoMetaCumplidaComponent,
+    TarjetaProductoCompradoComponent,
+    DetalleProductoComponent,
+    CarritoCompraComponent,
+    PagoComponent,
+    HeaderComponent,
+    FooterComponent,
+    PincipalComponent
+  ],
+  imports: [NgSimpleAutocompleteModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
